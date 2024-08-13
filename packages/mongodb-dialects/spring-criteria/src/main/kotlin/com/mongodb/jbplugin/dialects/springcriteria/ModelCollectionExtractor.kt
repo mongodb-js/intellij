f22b18519b89cb433b10e49b954512f30271251d
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-08-13 12:15:25 +0200

    chore: formatting

--- packages/mongodb-dialects/spring-criteria/src/main/kotlin/com/mongodb/jbplugin/dialects/springcriteria/ModelCollectionExtractor.kt
+++ packages/mongodb-dialects/spring-criteria/src/main/kotlin/com/mongodb/jbplugin/dialects/springcriteria/ModelCollectionExtractor.kt
@@ -9,24 +9,29 @@ object ModelCollectionExtractor {
     fun fromPsiClass(clazz: PsiClass): String? {
         val annotation = clazz.getAnnotation(AT_DOCUMENT_ANNOTATION)
         annotation?.let {
-val collectionName = annotation.findAttributeValue("value")?.tryToResolveAsConstantString().takeIf {
- !it
-.isNullOrBlank()
-}
- ?: annotation.findAttributeValue("collection")?.tryToResolveAsConstantString().takeIf { !it.isNullOrBlank() }
-collectionName?.let {
-return collectionName
-}
-/* it's the class name but with the first letter lower cased:
-https://docs.spring.io/spring-data/mongodb/docs/3.0.1.RELEASE/reference/html/#mongo-template.save-insert.collection
-*/
-val className = clazz.name!!
-return className[0].lowercaseChar() + className.substring(1)
-}
+            val collectionName = annotation.findAttributeValue("value")?.tryToResolveAsConstantString()
+                .takeIf { !it.isNullOrBlank() }
+                ?: annotation.findAttributeValue("collection")?.tryToResolveAsConstantString()
+                    .takeIf { !it.isNullOrBlank() }
+
+            collectionName?.let {
+                return collectionName
+            }
+
+            return toCollectionName(clazz)
+        }
 
         // not in the current class, check parent classes and interfaces
         return (clazz.interfaces + clazz.superClass)
             .filterNotNull()
             .firstNotNullOfOrNull { fromPsiClass(it) }
+    }
+
+    /** According to the documentation, it's the class name but with the first letter lower cased:
+     * https://docs.spring.io/spring-data/mongodb/docs/3.0.1.RELEASE/reference/html/
+     */
+    private fun toCollectionName(clazz: PsiClass): String {
+        val className = clazz.name!!
+        return className[0].lowercaseChar() + className.substring(1)
     }
 }
