0e949bc705aa2b9606f46921f8be4913ff191c43
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-08-13 11:58:17 +0200

    chore: extract collection from a model class

---
+++ packages/mongodb-dialects/spring-criteria/src/main/kotlin/com/mongodb/jbplugin/dialects/springcriteria/ModelCollectionExtractor.kt
@@ -0,0 +1,32 @@
+package com.mongodb.jbplugin.dialects.springcriteria
+
+import com.intellij.psi.PsiClass
+import com.mongodb.jbplugin.dialects.javadriver.glossary.tryToResolveAsConstantString
+
+private const val AT_DOCUMENT_ANNOTATION = "org.springframework.data.mongodb.core.mapping.Document"
+
+object ModelCollectionExtractor {
+    fun fromPsiClass(clazz: PsiClass): String? {
+        val annotation = clazz.getAnnotation(AT_DOCUMENT_ANNOTATION)
+        annotation?.let {
+val collectionName = annotation.findAttributeValue("value")?.tryToResolveAsConstantString().takeIf {
+ !it
+.isNullOrBlank()
+}
+ ?: annotation.findAttributeValue("collection")?.tryToResolveAsConstantString().takeIf { !it.isNullOrBlank() }
+collectionName?.let {
+return collectionName
+}
+/* it's the class name but with the first letter lower cased:
+https://docs.spring.io/spring-data/mongodb/docs/3.0.1.RELEASE/reference/html/#mongo-template.save-insert.collection
+*/
+val className = clazz.name!!
+return className[0].lowercaseChar() + className.substring(1)
+}
+
+        // not in the current class, check parent classes and interfaces
+        return (clazz.interfaces + clazz.superClass)
+            .filterNotNull()
+            .firstNotNullOfOrNull { fromPsiClass(it) }
+    }
+}
---
+++ packages/mongodb-dialects/spring-criteria/src/test/kotlin/com/mongodb/jbplugin/dialects/springcriteria/IntegrationTest.kt
@@ -0,0 +1,210 @@
+/**
+ * Class that contains the JUnit5 extension to run tests
+ * that use the IntelliJ Java parser.
+ */
+
+package com.mongodb.jbplugin.dialects.springcriteria
+
+import com.intellij.java.library.JavaLibraryUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.childrenOfType
+import com.intellij.testFramework.PsiTestUtil
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.*
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.mapping.Document
+import java.lang.reflect.Method
+import java.net.URI
+import java.net.URL
+import java.nio.file.Paths
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+
+@Retention(AnnotationRetention.RUNTIME)
+@Test
+annotation class ParsingTest(
+    val fileName: String,
+    @Language("java") val value: String,
+)
+
+/**
+ * Annotation to be used in the test, at the class level.
+ *
+ * @see com.mongodb.jbplugin.accessadapter.datagrip.adapter.DataGripMongoDbDriverTest
+ */
+@ExtendWith(IntegrationTestExtension::class)
+annotation class IntegrationTest
+
+/**
+ * Extension implementation. Must not be used directly.
+ */
+internal class IntegrationTestExtension :
+    BeforeAllCallback,
+    AfterAllCallback,
+    BeforeEachCallback,
+    InvocationInterceptor,
+    ParameterResolver {
+    private val namespace = ExtensionContext.Namespace.create(IntegrationTestExtension::class.java)
+    private val testFixtureKey = "TESTFIXTURE"
+    private val testPathKey = "TESTPATH"
+
+    override fun beforeAll(context: ExtensionContext) {
+        val projectFixture =
+            IdeaTestFixtureFactory
+                .getFixtureFactory()
+                .createLightFixtureBuilder(context.requiredTestClass.simpleName)
+                .fixture
+
+        val testFixture =
+            IdeaTestFixtureFactory
+                .getFixtureFactory()
+                .createCodeInsightFixture(
+                    projectFixture,
+                )
+
+        context.getStore(namespace).put(testFixtureKey, testFixture)
+        testFixture.setUp()
+
+        ApplicationManager.getApplication().invokeAndWait {
+            val module = testFixture.module
+
+            if (!JavaLibraryUtil.hasLibraryJar(module, "org.mongodb:mongodb-driver-sync:5.1.0")) {
+                runCatching {
+                    PsiTestUtil.addProjectLibrary(
+                        module,
+                        "org.springframework.data:spring-data-mongodb:4.3.2",
+                        listOf(pathToClassJarFile(MongoTemplate::class.java)),
+                    )
+
+                    PsiTestUtil.addProjectLibrary(
+                        module,
+                        "org.springframework.data:spring-data-mongodb-mapping:4.3.2",
+                        listOf(pathToClassJarFile(Document::class.java)),
+                    )
+                }
+            }
+        }
+
+        PsiTestUtil.addSourceRoot(testFixture.module, testFixture.project.guessProjectDir()!!)
+        val tmpRootDir = testFixture.tempDirFixture.getFile(".")!!
+        PsiTestUtil.addSourceRoot(testFixture.module, tmpRootDir)
+        context.getStore(namespace).put(testPathKey, tmpRootDir.path)
+    }
+
+    override fun beforeEach(context: ExtensionContext) {
+        val fixture = context.getStore(namespace).get(testFixtureKey) as CodeInsightTestFixture
+        val modulePath = context.getStore(namespace).get(testPathKey).toString()
+
+        ApplicationManager.getApplication().invokeAndWait {
+            val parsingTest = context.requiredTestMethod.getAnnotation(ParsingTest::class.java) ?: return@invokeAndWait
+
+            val fileName = Path(modulePath, "src", "main", "java", parsingTest.fileName).absolutePathString()
+
+            fixture.configureByText(
+                fileName,
+                parsingTest.value,
+            )
+        }
+    }
+
+    override fun interceptTestMethod(
+        invocation: InvocationInterceptor.Invocation<Void>,
+        invocationContext: ReflectiveInvocationContext<Method>,
+        extensionContext: ExtensionContext,
+    ) {
+        val throwable: AtomicReference<Throwable?> = AtomicReference(null)
+        val finished = AtomicBoolean(false)
+
+        val fixture = extensionContext.getStore(namespace).get(testFixtureKey) as CodeInsightTestFixture
+        val dumbService = DumbService.getInstance(fixture.project)
+
+        dumbService.runWhenSmart {
+            val result =
+                runCatching {
+                    invocation.proceed()
+                }
+            result.onSuccess {
+                finished.set(true)
+            }
+
+            result.onFailure {
+                finished.set(true)
+                throwable.set(it)
+            }
+        }
+
+        while (!finished.get()) {
+            Thread.sleep(1)
+        }
+
+        throwable.get()?.let {
+            it.printStackTrace(System.err)
+            throw it
+        }
+    }
+
+    override fun afterAll(context: ExtensionContext) {
+        val testFixture = context.getStore(namespace).get(testFixtureKey) as CodeInsightTestFixture
+
+        ApplicationManager.getApplication().invokeAndWait {
+            testFixture.tearDown()
+        }
+    }
+
+    override fun supportsParameter(
+        parameterContext: ParameterContext,
+        extensionContext: ExtensionContext,
+    ): Boolean =
+        parameterContext.parameter.type == Project::class.java ||
+            parameterContext.parameter.type == CodeInsightTestFixture::class.java ||
+            parameterContext.parameter.type == PsiFile::class.java ||
+            parameterContext.parameter.type == JavaPsiFacade::class.java
+
+    override fun resolveParameter(
+        parameterContext: ParameterContext,
+        extensionContext: ExtensionContext,
+    ): Any {
+        val fixture = extensionContext.getStore(namespace).get(testFixtureKey) as CodeInsightTestFixture
+
+        return when (parameterContext.parameter.type) {
+            Project::class.java -> fixture.project
+            CodeInsightTestFixture::class.java -> fixture
+            PsiFile::class.java -> fixture.file
+            JavaPsiFacade::class.java -> JavaPsiFacade.getInstance(fixture.project)
+            else -> TODO("Parameter of type ${parameterContext.parameter.type.canonicalName} is not supported.")
+        }
+    }
+
+    private fun pathToClassJarFile(javaClass: Class<*>): String {
+        val classResource: URL =
+            javaClass.getResource(javaClass.getSimpleName() + ".class")
+                ?: throw RuntimeException("class resource is null")
+        val url: String = classResource.toString()
+        if (url.startsWith("jar:file:")) {
+            // extract 'file:......jarName.jar' part from the url string
+            val path = url.replace("^jar:(file:.*[.]jar)!/.*".toRegex(), "$1")
+            try {
+                return Paths.get(URI(path)).toString()
+            } catch (e: Exception) {
+                throw RuntimeException("Invalid Jar File URL String")
+            }
+        }
+        throw RuntimeException("Invalid Jar File URL String")
+    }
+}
+
+fun PsiFile.getClassByName(name: String): PsiClass =
+    childrenOfType<PsiClass>().first {
+        it.name == name
+    }
