3df97a38e4a65c5588e132962c32be56f28d89cf
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-08-21 17:00:26 +0200

    chore: support for simple query expressions

--- packages/mongodb-dialects/spring-criteria/src/main/kotlin/com/mongodb/jbplugin/dialects/springcriteria/SpringCriteriaDialectParser.kt
+++ packages/mongodb-dialects/spring-criteria/src/main/kotlin/com/mongodb/jbplugin/dialects/springcriteria/SpringCriteriaDialectParser.kt
@@ -1,10 +1,16 @@
 package com.mongodb.jbplugin.dialects.springcriteria
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiExpression
 import com.intellij.psi.PsiMethodCallExpression
 import com.mongodb.jbplugin.dialects.DialectParser
+import com.mongodb.jbplugin.dialects.javadriver.glossary.tryToResolveAsConstant
+import com.mongodb.jbplugin.dialects.javadriver.glossary.tryToResolveAsConstantString
 import com.mongodb.jbplugin.mql.Node
+import com.mongodb.jbplugin.mql.components.HasChildren
+import com.mongodb.jbplugin.mql.components.HasFieldReference
+import com.mongodb.jbplugin.mql.components.HasValueReference
+import com.mongodb.jbplugin.mql.components.Named
+import com.mongodb.jbplugin.mql.toBsonType
 
 private const val CRITERIA_CLASS_FQN = "org.springframework.data.mongodb.core.query.Criteria"
 
@@ -15,22 +21,59 @@ object SpringCriteriaDialectParser : DialectParser<PsiElement> {
 
     override fun parse(source: PsiElement): Node<PsiElement> {
         val criteriaChain = source.findCriteriaWhereExpression() ?: return Node(source, emptyList())
+        return Node(source, listOf(HasChildren(parseQueryRecursively(criteriaChain))))
+    }
 
-        return Node(source, emptyList())
+    private fun parseQueryRecursively(fieldNameCall: PsiMethodCallExpression): List<Node<PsiElement>> {
+        if (!fieldNameCall.isACriteriaQueryMethod()) {
+            return emptyList()
+        }
+
+        val valueCall = fieldNameCall.parent.parent as? PsiMethodCallExpression ?: return emptyList()
+
+        val fieldName = fieldNameCall.argumentList.expressions[0].tryToResolveAsConstantString()!!
+        val value = valueCall.argumentList.expressions[0].tryToResolveAsConstant()!!
+        val name = valueCall.resolveMethod()?.name!!
+
+        val fieldReference = HasFieldReference(
+            HasFieldReference.Known(fieldNameCall.argumentList.expressions[0], fieldName)
+        )
+
+        val valueReference = HasValueReference(
+            HasValueReference.Constant(value, value.javaClass.toBsonType(value))
+        )
+
+        val predicate = Node<PsiElement>(fieldNameCall, listOf(
+            Named(name),
+            fieldReference,
+            valueReference
+        ))
+
+        if (valueCall.parent.parent is PsiMethodCallExpression) {
+            val nextField = valueCall.parent.parent as PsiMethodCallExpression
+            return listOf(predicate) + parseQueryRecursively(nextField)
+        }
+
+        return listOf(predicate)
     }
 }
 
-private fun PsiElement.findCriteriaWhereExpression(): PsiExpression? {
+private fun PsiElement.findCriteriaWhereExpression(): PsiMethodCallExpression? {
     val methodCalls = findAllChildrenOfType(PsiMethodCallExpression::class.java)
-    var bottomLevel: PsiExpression = methodCalls.find { methodCall ->
+    var bottomLevel: PsiMethodCallExpression = methodCalls.find { methodCall ->
         val method = methodCall.resolveMethod() ?: return@find false
         method.containingClass?.qualifiedName == CRITERIA_CLASS_FQN &&
                 method.name == "where"
     } ?: return null
 
     while (bottomLevel.text.startsWith("where")) {
-        bottomLevel = (bottomLevel.parent as? PsiExpression) ?: return bottomLevel
+        bottomLevel = (bottomLevel.parent as? PsiMethodCallExpression) ?: return bottomLevel
     }
 
     return bottomLevel
+}
+
+private fun PsiMethodCallExpression.isAcriteriaQueryMethod(): Boolean {
+    val method = resolveMethod() ?: return false
+    return method.containingClass?.qualifiedName == CRITERIA_CLASS_FQN
 }
--- packages/mongodb-dialects/spring-criteria/src/test/kotlin/com/mongodb/jbplugin/dialects/springcriteria/SpringCriteriaDialectParserTest.kt
+++ packages/mongodb-dialects/spring-criteria/src/test/kotlin/com/mongodb/jbplugin/dialects/springcriteria/SpringCriteriaDialectParserTest.kt
@@ -44,12 +44,68 @@ class Repository {
 
         val whereReleasedIsTrue = node.component<HasChildren<PsiElement>>()!!.children[0]
         val fieldNameReference = whereReleasedIsTrue.component<HasFieldReference<PsiElement>>()!!.reference
-        val valueReference = node.component<HasValueReference>()!!.reference
+        val valueReference = whereReleasedIsTrue.component<HasValueReference>()!!.reference
 
         fieldNameReference as HasFieldReference.Known<PsiElement>
         valueReference as HasValueReference.Constant
 
         assertEquals("released", fieldNameReference.fieldName)
         assertEquals(true, valueReference.value)
+    }
+
+    @ParsingTest(
+        fileName = "Book.java",
+        """
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+@Document
+record Book() {}
+
+class Repository {
+    private final MongoTemplate template;
+    
+    public Repository(MongoTemplate template) {
+        this.template = template;
+    }
+    
+    public List<Book> allReleasedBooks() {
+        return template.query(Book.class)
+                       .matching(where("released").is(true).and("hidden").is(0))
+                       .all();
+    }
+}
+        """
+    )
+    fun `extracts a criteria query with multiple fields`(
+        psiFile: PsiFile
+    ) {
+        val query = psiFile.getQueryAtMethod("Repository", "allReleasedBooks")
+        val node = SpringCriteriaDialectParser.parse(query)
+
+        val whereReleasedIsTrue = node.component<HasChildren<PsiElement>>()!!.children[0]
+        val referenceToReleased = whereReleasedIsTrue.component<HasFieldReference<PsiElement>>()!!.reference
+        val referenceToTrue = whereReleasedIsTrue.component<HasValueReference>()!!.reference
+
+        val whereHiddenIs0 = node.component<HasChildren<PsiElement>>()!!.children[1]
+        val referenceToHidden = whereHiddenIs0.component<HasFieldReference<PsiElement>>()!!.reference
+        val referenceTo0 = whereHiddenIs0.component<HasValueReference>()!!.reference
+
+        referenceToReleased as HasFieldReference.Known<PsiElement>
+        referenceToTrue as HasValueReference.Constant
+
+        referenceToHidden as HasFieldReference.Known<PsiElement>
+        referenceTo0 as HasValueReference.Constant
+
+        assertEquals("released", referenceToReleased.fieldName)
+        assertEquals(true, referenceToTrue.value)
+
+        assertEquals("hidden", referenceToHidden.fieldName)
+        assertEquals(0, referenceTo0.value)
     }
 }
