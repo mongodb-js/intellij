cf47e6f4a6ddf132b392dd457dd67cf678742127
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-09-13 15:38:00 +0200

    chore: revert to main

--- packages/jetbrains-plugin/src/test/resources/project-fixtures/basic-java-project-with-mongodb/src/main/java/alt/mongodb/springcriteria/SpringCriteriaRepository.java
+++ packages/jetbrains-plugin/src/test/resources/project-fixtures/basic-java-project-with-mongodb/src/main/java/alt/mongodb/springcriteria/SpringCriteriaRepository.java
@@ -3,7 +3,6 @@ package alt.mongodb.springcriteria;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
 import java.util.List;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
@@ -21,9 +20,9 @@ public class SpringCriteriaRepository {
         this.template = template;
     }
 
-    private List<Movie> allMoviesWithRatingAtLeastReleasedAtLeastAt(int rating, Date releaseDate) {
+    private List<Movie> allMoviesWithRatingAtLeast(int rating) {
         return template.find(
-                query(where("awards.wins").gte(rating).and("released").gte(releaseDate)),
+                query(where( "tomatoes.viewer.rating").gte(rating)),
                 Movie.class
         );
     }
