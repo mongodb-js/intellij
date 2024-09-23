0da3fd7a0c3410a4f8f4a640225e28984ec6cbb7
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-09-23 22:20:10 +0200

    chore: test ktlint (this should fail)

--- packages/jetbrains-plugin/src/main/kotlin/com/mongodb/jbplugin/ActivatePluginPostStartupActivity.kt
+++ packages/jetbrains-plugin/src/main/kotlin/com/mongodb/jbplugin/ActivatePluginPostStartupActivity.kt
@@ -24,14 +24,9 @@ import kotlinx.coroutines.launch
 /**
  * Class that represents the link that opens the settings page for MongoDB.
  */
-class OpenMongoDbPluginSettingsAction : AnAction(
-    TelemetryMessages.message("action.disable-telemetry")
-) {
+class OpenMongoDbPluginSettingsAction : AnAction(TelemetryMessages.message("action.disable-telemetry")) {
     override fun actionPerformed(event: AnActionEvent) {
-        ShowSettingsUtil.getInstance().showSettingsDialog(
-            event.project,
-            PluginSettingsConfigurable::class.java
-        )
+        ShowSettingsUtil.getInstance().showSettingsDialog(event.project, PluginSettingsConfigurable::class.java)
     }
 }
 
@@ -49,14 +44,10 @@ class OpenPrivacyPolicyPage : AnAction(TelemetryMessages.message("action.view-pr
  *
  * @param cs
  */
-class ActivatePluginPostStartupActivity(
-    private val cs: CoroutineScope
-) : StartupActivity, DumbAware {
+class ActivatePluginPostStartupActivity(private val cs: CoroutineScope) : StartupActivity, DumbAware {
     override fun runActivity(project: Project) {
         cs.launch {
-            val pluginActivated = ApplicationManager.getApplication().getService(
-                PluginActivatedProbe::class.java
-            )
+            val pluginActivated = ApplicationManager.getApplication().getService(PluginActivatedProbe::class.java)
             pluginActivated.pluginActivated()
 
             val settings = useSettings()
