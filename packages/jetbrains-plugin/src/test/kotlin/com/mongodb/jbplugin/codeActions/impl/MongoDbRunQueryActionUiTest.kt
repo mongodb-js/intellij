61fc8b9550c1ae1d1ca9a1d4b3a10df5a0779814
Himanshu Singh <himanshu.singhs@outlook.in>
2024-10-17 16:14:50 +0200

    chore: re-enable other test for testing

--- packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/codeActions/impl/MongoDbRunQueryActionUiTest.kt
+++ packages/jetbrains-plugin/src/test/kotlin/com/mongodb/jbplugin/codeActions/impl/MongoDbRunQueryActionUiTest.kt
@@ -2,12 +2,15 @@ package com.mongodb.jbplugin.codeActions.impl
 
 import com.intellij.remoterobot.RemoteRobot
 import com.mongodb.jbplugin.fixtures.*
+import com.mongodb.jbplugin.fixtures.components.findJavaEditorToolbar
 import com.mongodb.jbplugin.fixtures.components.findJavaEditorToolbarPopup
 import com.mongodb.jbplugin.fixtures.components.findRunQueryGutter
 import com.mongodb.jbplugin.fixtures.components.idea.ideaFrame
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
 
 @UiTest
 @RequiresMongoDbCluster
@@ -36,24 +39,24 @@ class MongoDbRunQueryActionUiTest {
         popup.cancel()
     }
 
-    // @Test
-    // @RequiresProject("basic-java-project-with-mongodb", smartMode = true)
-    // fun `opens the popup, connects and opens the datagrip console`(remoteRobot: RemoteRobot) {
-    //     remoteRobot.ideaFrame().openFile(
-    //         "/src/main/java/alt/mongodb/javadriver/JavaDriverRepository.java"
-    //     )
-    //     remoteRobot.findJavaEditorToolbar().detachDataSource()
-    //     remoteRobot.findRunQueryGutter(atLine = 24)!!.click()
-    //     // because we are disconnected, we should now try to connect
-    //     val popup = remoteRobot.findJavaEditorToolbarPopup()
-    //     popup.dataSources.selectItem(
-    //         javaClass.simpleName
-    //     )
-    //     popup.ok("Run Query", timeout = 1.minutes)
-    //     // check that we open a console
-    //     eventually {
-    //         val currentEditor = remoteRobot.ideaFrame().currentTab()
-    //         assertTrue(currentEditor.editor.fileName.startsWith("console"))
-    //     }
-    // }
+    @Test
+    @RequiresProject("basic-java-project-with-mongodb", smartMode = true)
+    fun `opens the popup, connects and opens the datagrip console`(remoteRobot: RemoteRobot) {
+        remoteRobot.ideaFrame().openFile(
+            "/src/main/java/alt/mongodb/javadriver/JavaDriverRepository.java"
+        )
+        remoteRobot.findJavaEditorToolbar().detachDataSource()
+        remoteRobot.findRunQueryGutter(atLine = 24)!!.click()
+        // because we are disconnected, we should now try to connect
+        val popup = remoteRobot.findJavaEditorToolbarPopup()
+        popup.dataSources.selectItem(
+            javaClass.simpleName
+        )
+        popup.ok("Run Query", timeout = 1.minutes)
+        // check that we open a console
+        eventually {
+            val currentEditor = remoteRobot.ideaFrame().currentTab()
+            assertTrue(currentEditor.editor.fileName.startsWith("console"))
+        }
+    }
 }
